866a890bb8fb9d4faf03b4c4bed088557b9acfcf
JS314 <149881288+JamesSykes314@users.noreply.github.com>
2024-09-05 14:40:08 +0100

    slightly improved plotting.py

--- plotting.py
+++ plotting.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-results = pd.read_csv('results_df')
-plt.hist([results.iloc[0,:], results.iloc[1,:]], label=['LHS', 'BO'], rwidth=0.6)
+
+results = pd.read_csv('CSV_results/results_df_1')
+rows_to_plot = [0,1]
+sampling_methods = ["LHS", "BO"]
+
+plt.hist([results.iloc[rows_to_plot[0],3:].dropna(), results.iloc[rows_to_plot[1],3:].dropna()], rwidth=0.6, label=[sampling_methods[0], sampling_methods[1]])
 plt.legend(loc='upper left', framealpha=0.25)
 plt.title("Log10(hypervolume)")
-plt.show()
+plt.savefig('plot.png', format = 'png')
