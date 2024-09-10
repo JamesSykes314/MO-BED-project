eeb8c1fbc36ad98012f219738008fc90ceb5e3ab
JS314 <149881288+JamesSykes314@users.noreply.github.com>
2024-09-10 21:26:37 +0100

    added SMILE series to features dataframe function

--- SMILE_to_features.py
+++ SMILE_to_features.py
@@ -3,9 +3,9 @@ from rdkit.Chem import Descriptors
 import numpy as np
 import pandas as pd
 
-def SMILE_to_desc_dict(SMILE, descriptors_set = "default"):
+def SMILE_to_desc_dict(SMILE, descriptors_set="original"):
     mol = Chem.MolFromSmiles(SMILE)
-    if descriptors_set == "default":
+    if descriptors_set == "original":
         desc = {
             "SMILES": SMILE,
             "Molecular Weight": Descriptors.MolWt(mol),
@@ -49,7 +49,14 @@ def SMILE_to_desc_dict(SMILE, descriptors_set = "default"):
             "Heavy Atoms": Descriptors.HeavyAtomCount(mol)
         }
     else:
-        print("Must choose either default, ZINC or ZINC expanded for the descriptor set.")
+        print("Must choose either original, ZINC or ZINC expanded for the descriptor set.")
     return desc
 
 # print(SMILE_to_desc_dict("O=C(O)CCCc1ccc(N(CCCl)CCCl)cc1", "ZINC"))
+
+
+def SMILE_series_to_features_df(SMILE_series, descriptors_set="original", drop_duplicates=True):
+    features_df = pd.DataFrame(list(SMILE_series.apply(SMILE_to_desc_dict, descriptors_set=descriptors_set)))
+    if drop_duplicates:
+        features_df = features_df.drop_duplicates(subset=features_df.columns[1:])
+    return features_df
