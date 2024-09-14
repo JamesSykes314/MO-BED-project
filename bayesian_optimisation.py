7908e7dde29f5fc8e8b9904df7dd12b6397fb58c
JS314 <149881288+JamesSykes314@users.noreply.github.com>
2024-09-14 18:00:50 +0100

    improved bayesian_optimisation.py to allow function_numpy_conversion to be used for functions with single inputs

--- bayesian_optimisation.py
+++ bayesian_optimisation.py
@@ -12,12 +12,16 @@ def function_numpy_conversion(func):
             X.shape[1]
         except:
             X = np.array(X)
-        if len(X.shape)<2:
+        if len(X.shape) < 2:
             X = np.expand_dims(X, axis=0)
         y = np.array([], dtype=float)
         for i in range(X.shape[0]):
-            ynew = func(X[i,:].tolist())
-            y = np.append(y, ynew)
+            if X.shape[1] == 1:
+                y_new = func(X[i, 0])
+                y = np.append(y, y_new)
+            else:
+                y_new = func(X[i, :].tolist())
+                y = np.append(y, y_new)
         y = y.reshape(X.shape[0],-1)
         return y
     return new_func
@@ -48,14 +52,15 @@ def run_single_obj_experiment(objective_function, parameter_list, sampling_metho
     Exp.set_optim_specs(objective_func=objective_func, maximize=maximise)
 
     opt_check_freq = 20
-    opt_array = np.array(np.zeros((1+(n_trials-1)//opt_check_freq, n_dim+1)))
+    opt_array_rows = 2+(n_trials-1)//opt_check_freq
+    opt_array = np.array(np.zeros((opt_array_rows, n_dim+1)))
     for i in range(n_trials):
         if i % 20 == 0:
             print("{} trials completed".format(i))
         if i % opt_check_freq == 0:
             Y_optim, X_optim, _ = Exp.get_optim()
-            opt_array[i//opt_check_freq,:n_dim] = X_optim
-            opt_array[i//opt_check_freq,n_dim] = Y_optim
+            opt_array[i//opt_check_freq, :n_dim] = X_optim
+            opt_array[i//opt_check_freq, n_dim] = Y_optim
             print(X_optim, Y_optim)
         # Generate the next experiment point
         X_new, X_new_real, acq_func = Exp.generate_next_point(acq_func_name=acq_function, n_candidates=1)
@@ -67,5 +72,7 @@ def run_single_obj_experiment(objective_function, parameter_list, sampling_metho
     final_time = time.time()
     time_taken = final_time - start_time
     Y_optim, X_optim, _ = Exp.get_optim()
+    opt_array[opt_array_rows-1, :n_dim] = X_optim
+    opt_array[opt_array_rows-1, n_dim] = Y_optim
     print(opt_array)
     return X_optim, Y_optim, time_taken
