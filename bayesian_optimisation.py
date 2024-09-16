5f317f43a171db642d211c24f37ddc8159c96fe1
James Sykes <jameslsykes@icloud.com>
2024-09-16 11:14:10 +0100

    added SLURM job scripts and outputs

--- bayesian_optimisation.py
+++ bayesian_optimisation.py
@@ -26,7 +26,7 @@ def function_numpy_conversion(func):
         return y
     return new_func
 
-def run_single_obj_experiment(objective_function, parameter_list, sampling_method, n_init, n_trials, acq_function="EI", kernel="default", maximise=False, plotting_flag=False):
+def run_single_obj_experiment(objective_function, parameter_list, sampling_method, n_init, n_trials, acq_function="EI", kernel="default", maximise=False, plotting_flag=False, save_fig_flag=False):
     start_time = time.time()
     Exp = bo.Experiment('Experiment')
     Exp.define_space(parameter_list)
@@ -75,10 +75,10 @@ def run_single_obj_experiment(objective_function, parameter_list, sampling_metho
             print('Iteration {}, objective function'.format(i + 1))
             Y_new = bo.eval_objective_func(X_new, Exp.X_ranges, Exp.objective_func)
             Exp.objective_func = None
-            plotting.response_1d_exp(Exp, X_new=X_new, Y_new=Y_new, mesh_size=100, plot_real=True, save_fig=False)
+            plotting.response_1d_exp(Exp, X_new=X_new, Y_new=Y_new, mesh_size=100, plot_real=True, save_fig=save_fig_flag)
             Exp.objective_func = duplicate_obj_func
             print('Iteration {}, acquisition function'.format(i + 1))
-            plotting.acq_func_1d_exp(Exp, X_new=X_new, mesh_size=100, save_fig=False)
+            plotting.acq_func_1d_exp(Exp, X_new=X_new, mesh_size=100, save_fig=save_fig_flag)
         # Retrain the model by input the next point into Exp object
         Exp.run_trial(X_new, X_new_real, Y_new_real)
 
