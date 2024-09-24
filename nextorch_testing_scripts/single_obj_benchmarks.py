ab49a8b818f6253e7b881599c7cddd3d818c07ea
JS314 <149881288+JamesSykes314@users.noreply.github.com>
2024-09-24 23:36:29 +0100

    Added initial attempt at function which fixes arguments of (a now more flexible) function that performs lots of iterations of BO on benchmark fns.

--- nextorch_testing_scripts/single_obj_benchmarks.py
+++ nextorch_testing_scripts/single_obj_benchmarks.py
@@ -4,11 +4,9 @@ import statistics
 import bayesian_optimisation as bayopt
 from nextorch import plotting, bo, doe, utils, io, parameter
 
-def run_bayes_opt_benchmark_fns(n_trials):
-    n_dims = 2
-    func = bf.Ackley(n_dimensions=n_dims)
-    total_samples = 200
-    iterations = int(round(10000/total_samples))
+
+def mean_min_benchmark(bench_func, n_dims, total_samples, n_trials, iterations):
+    func = bench_func(n_dimensions=n_dims)
     print("Function is {} ({} dimensions). Total samples in each optimisation process is {} and we are running {} "
           "iterations of this each time we test a point (the number of trials).".format(func, n_dims, total_samples, iterations))
     # Note: this should be changed so that it goes outside of this function. There should be another function with
@@ -30,6 +28,18 @@ def run_bayes_opt_benchmark_fns(n_trials):
             print("Error: {}".format(e))
     return statistics.fmean(min_values)
 
+# Need to make sure the images go into folders that contain info on what the arguments were/or automatically go into the right folder structure
 
-param = parameter.Parameter(x_type="ordinal", x_range=[0, 96], interval=1)
-print(bayopt.run_single_obj_experiment(run_bayes_opt_benchmark_fns, parameter_list=[param], sampling_method="LHS", n_init=3, n_trials=10, plotting_flag=True, save_fig_flag=True))
+
+def choose_mean_min_bench_args(func, n_dims, total_samples, iterations="default"):
+    if iterations == "default":
+        iterations = int(round(10000 / total_samples))
+    def new_function(n_trials):
+        return mean_min_benchmark(func, n_dims, total_samples, n_trials, iterations)
+    return new_function
+
+
+if __name__ == "__main__":
+    param = parameter.Parameter(x_type="ordinal", x_range=[0, 96], interval=1)
+    run_bayes_opt_benchmark_fns = choose_mean_min_bench_args(bf.Ackley, 2, 200)
+    print(bayopt.run_single_obj_experiment(run_bayes_opt_benchmark_fns, parameter_list=[param], sampling_method="LHS", n_init=3, n_trials=10, plotting_flag=True, save_fig_flag=True))
