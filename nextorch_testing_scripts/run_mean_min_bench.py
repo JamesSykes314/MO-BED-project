28cf113c1b9a0a860d67224948f9e240ab5c7327
James Sykes <jameslsykes@icloud.com>
2024-09-27 14:34:33 +0100

    Added SLURM outputs from more overnight tests and added the files 'a' and 'A' to test how they appear on my local machine.

--- nextorch_testing_scripts/run_mean_min_bench.py
+++ nextorch_testing_scripts/run_mean_min_bench.py
@@ -12,10 +12,10 @@ parser.add_argument('--total_samples', type=int, required=True, help='Total numb
 args = parser.parse_args()
 
 desired_func = getattr(bf, args.bench_func)
-iterations = int(round(10000 / args.total_samples))
+iterations = int(round(7500 / args.total_samples))
 run_bayes_opt_benchmark_fns = sob.choose_mean_min_bench_args(desired_func, args.n_dims, args.total_samples, iterations)
 param = parameter.Parameter(x_type="ordinal", x_range=[0, args.total_samples - 4], interval=1)
 experiment_name = f"{args.bench_func}_{args.n_dims}_{args.total_samples}_{iterations}"
 print(bayopt.run_single_obj_experiment(run_bayes_opt_benchmark_fns,
                                        parameter_list=[param], sampling_method="LHS", n_init=15, n_trials=10,
-                                       plotting_flag=False, save_final_fig=True, exp_name=experiment_name))
+                                       plotting_flag=True, save_all_figs=True, save_final_fig=True, exp_name=experiment_name))
